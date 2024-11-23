ce8c32be552814a58885104d0d8597065c034043
Fulton Browne <fultonbrowne@pm.me>
2024-11-23 09:17:19 -0600

    Initial Commit

---
+++ metal fun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  metal fun
+//
+//  Created by Fulton Browne on 11/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ metal fun/metal_funApp.swift
@@ -0,0 +1,17 @@
+//
+//  metal_funApp.swift
+//  metal fun
+//
+//  Created by Fulton Browne on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct metal_funApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
